1f8b224ddfbfad1178bcfae5f3bb0997ae205d7b
hoaaaah <hoabh59@gmail.com>
2025-07-21 15:45:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,17 @@ window.addEventListener("load", () => {
       main.style.display = "block";
     }
   }, 2500); // Hiện logo trong 2.5 giây
+  true });
+  });
+window.addEventListener('load', () => {
+  const sound = document.getElementById('introSound');
+  sound.play().catch(() => {
+    document.addEventListener('click', () => sound.play(), { once: true });
+  });
+
+  setTimeout(() => {
+    document.querySelector('.logo-wrapper').style.display = 'none';
+    document.querySelector('.main-content').style.display = 'block';
+  }, 2500);
 });
+
